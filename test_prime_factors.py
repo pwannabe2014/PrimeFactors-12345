0337761356776610fe778b6c0a7705f02fdfb4fc
dhkang <pwannabe2014@gmail.com>
2025-07-31 12:37:45 +0900

    [feature] p.31

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -6,3 +6,8 @@ from prime_factors import PrimeFactor
 def test_prime_factor_of_1():
     prime_factor = PrimeFactor()
     assert prime_factor.of(1) == []
+
+
+def test_prime_factor_of_2():
+    prime_factor = PrimeFactor()
+    assert prime_factor.of(2) == [2]
